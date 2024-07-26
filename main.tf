14ae252321f35ef5736aa5a4a6669ea12769724e
Ryan Goh <1871494+ryanoolala@users.noreply.github.com>
2024-07-26 18:20:41 +0800

    chore: updated to v4.0.6

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_eip" "nat" {
 
 # virtual private cloud creator
 module "vpc" {
-  source = "github.com/GovTechSG/terraform-aws-vpc-forked?ref=v4.0.5"
+  source = "github.com/GovTechSG/terraform-aws-vpc-forked?ref=v4.0.6"
 
   # meta data
   name                  = var.vpc_name
